b29ba4a319c78e6df0efeb236c2077af333a662e
Qiong Zhou <justin.qiong@gmail.com>
2024-01-15 11:12:38 +0800

    remove __pycache__

--- 2024/jan/714/TestSolution.py
+++ 2024/jan/714/TestSolution.py
@@ -7,3 +7,4 @@ class TestSolution(TestCase):
         solution = Solution()
         self.assertEqual(8, solution.maxProfit([1, 3, 2, 8, 4, 9], 2))
         self.assertEqual(6, solution.maxProfit([1, 3, 7, 5, 10, 3], 3))
+        self.assertEqual(3, solution.maxProfit([1, 6], 2))
