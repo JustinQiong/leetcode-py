fecb72fd64428a4c210e9bca12bb87145b0b4aa0
Qiong Zhou <justin.qiong@gmail.com>
2024-01-15 14:03:15 +0800

    208. Implement Trie (Prefix Tree)

---
+++ 2024/jan/208/Trie.py
@@ -0,0 +1,38 @@
+"""
+208. Implement Trie (Prefix Tree)
+Each node of the Trie has an array with size 26.
+Each of the element in the array indicates if the char exists or not.
+If the element is not None. Then search the children of the element
+until we get a None.
+"""
+class Trie:
+
+    def __init__(self):
+        self.children = [None] * 26
+        self.isEnd = False
+
+    def insert(self, word: str) -> None:
+        node = self
+        for ch in word:
+            ch = ord(ch) - ord("a")
+            if not node.children[ch]:
+                node.children[ch] = Trie()
+            node = node.children[ch]
+        node.isEnd = True
+
+    def searchPrefix(self, prefix: str) -> "Trie":
+        node = self
+        for ch in prefix:
+            ch = ord(ch) - ord("a")
+            if not node.children[ch]:
+                return None
+            node = node.children[ch]
+        return node
+
+    def search(self, word: str) -> bool:
+        node = self.searchPrefix(word)
+        return node is not None and node.isEnd
+
+    def startsWith(self, prefix: str) -> bool:
+        return self.searchPrefix(prefix) is not None
+
