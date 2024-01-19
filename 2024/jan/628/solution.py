6f4eae165cd2d2341c4bd490e60899c020b781b4
Qiong Zhou <justin.qiong@gmail.com>
2024-01-19 08:47:24 +0800

    628. Maximum Product of Three Numbers

---
+++ 2024/jan/628/solution.py
@@ -0,0 +1,31 @@
+from math import inf
+from typing import List
+
+"""
+628. Maximum Product of Three Numbers
+The maximum product may be the first and second minimum negative integer multiple 
+the maximum positive integer or the maximum three positive integers multiple together.
+"""
+class Solution:
+    def maximumProduct(self, nums: List[int]) -> int:
+        min1, min2 = inf, inf
+        max1, max2, max3 = -inf, -inf, -inf
+
+        for x in nums:
+            if x < min1:
+                min2 = min1
+                min1 = x
+            elif x < min2:
+                min2 = x
+
+            if x > max1:
+                max3 = max2
+                max2 = max1
+                max1 = x
+            elif x > max2:
+                max3 = max2
+                max2 = x
+            elif x > max3:
+                max3 = x
+
+        return max(min1 * min2 * max1, max1 * max2 * max3)
