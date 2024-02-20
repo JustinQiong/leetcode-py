4e6cc863cac53a829c93bea0e01d51351864f145
Qiong Zhou <justin.qiong@gmail.com>
2024-02-20 11:00:05 +0800

    628. Maximum Product of Three Numbers
    simplify the code

--- 2024/jan/628/solution.py
+++ 2024/jan/628/solution.py
@@ -13,19 +13,14 @@ class Solution:
 
         for x in nums:
             if x < min1:
-                min2 = min1
-                min1 = x
+                min2,min1 = min1,x
             elif x < min2:
                 min2 = x
 
             if x > max1:
-                max3 = max2
-                max2 = max1
-                max1 = x
+                max3,max2,max1 = max2,max1, x
             elif x > max2:
-                max3 = max2
-                max2 = x
+                max3, max2 = max2, x
             elif x > max3:
                 max3 = x
-
-        return max(min1 * min2 * max1, max1 * max2 * max3)
+        return max(min1*min2*max1, max1*max2*max3)
