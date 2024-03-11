5f208fe65f37050e8b9d001cdc3c71d0c8852c77
Qiong Zhou <justin.qiong@gmail.com>
2024-03-11 09:29:19 +0800

    2129. Capitalize the Title

---
+++ 2024/mar/2129/solution.py
@@ -0,0 +1,13 @@
+"""
+2129. Capitalize the Title
+"""
+class Solution:
+    def capitalizeTitle(self, title: str) -> str:
+        res = []
+        for word in title.split():
+            if len(word) <= 2:
+                res.append(word.lower())
+            else:
+                res.append(word[0].upper() + word[1:].lower())
+
+        return ' '.join(res)
